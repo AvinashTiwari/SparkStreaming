1abe13b19cdf0d2098251119c08fc39071b9fb9a
Avinash Tiwari <qwe123kids@gmail.com>
2018-01-11 19:36:57 -0700

    Spark Streaming

---
+++ SparkStreamSample/src/main/java/com/avinash/spark/streamexample/SparkStreamSampleApplication.java
@@ -0,0 +1,13 @@
+package com.avinash.spark.streamexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+@SpringBootApplication
+public class SparkStreamSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkStreamSampleApplication.class, args);
+	}
+}
+*/
---
+++ SparkStreamSampleSocketServer/src/test/java/com/avinash/spark/stream/socket/example/SparkStreamSampleSocketServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avinash.spark.stream.socket.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SparkStreamSampleSocketServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
